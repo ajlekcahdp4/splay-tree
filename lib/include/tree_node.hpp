7928a4afe4848a4f1c8d4f13effb95a450e84cb2
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2022-11-06 21:37:59 +0300

    add separate files for node structures

---
+++ lib/include/tree_node.hpp
@@ -0,0 +1,118 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <alex.rom23@mail.ru> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Alex Romanov
+ * ----------------------------------------------------------------------------
+ */
+
+// node structs for binary tree
+
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <cstddef>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <tuple>
+#include <utility>
+
+namespace red
+{
+namespace containers
+{
+
+struct dl_binary_tree_node_base
+{
+  private:
+    dl_binary_tree_node_base *m_parent = nullptr;
+    dl_binary_tree_node_base *m_left   = nullptr;
+    dl_binary_tree_node_base *m_right  = nullptr;
+
+  public:
+    virtual ~dl_binary_tree_node_base () {};
+
+    dl_binary_tree_node_base *minimum ()
+    {
+        auto node = this;
+        while ( node->m_left )
+            node = node->m_left;
+        return node;
+    };
+
+    dl_binary_tree_node_base *maximum ()
+    {
+        auto node = this;
+        while ( node->m_right )
+            node = node->m_right;
+        return node;
+    };
+
+    dl_binary_tree_node_base *successor () const;
+    dl_binary_tree_node_base *predecessor () const;
+
+    bool is_left_child () const { return (m_parent ? this == m_parent->m_left : false); }
+    bool is_linear () const { return m_parent && (is_left_child () == m_parent->is_left_child ()); }
+
+    virtual dl_binary_tree_node_base *rotate_left ();
+    virtual dl_binary_tree_node_base *rotate_right ();
+    dl_binary_tree_node_base *rotate_to_parent ()
+    {
+        if ( is_left_child () )
+            return m_parent->rotate_right ();
+        else
+            return m_parent->rotate_left ();
+    }
+};
+
+struct do_tree_node_base
+{
+    using size_type     = std::size_t;
+    using base_node_ptr = do_tree_node_base *;
+    using self          = do_tree_node_base;
+    using owning_ptr    = typename std::unique_ptr<do_tree_node_base>;
+
+    size_type m_size = 1;
+
+    static size_type size (dl_binary_tree_node_base *node) { return (node ? node->m_size : 0); }
+
+    dl_binary_tree_node_base *m_minimum ()
+    {
+        dl_binary_tree_node_base *x = this;
+        while ( x->m_left.get () )
+            x = x->m_left.get ();
+        return x;
+    }
+
+    dl_binary_tree_node_base *m_maximum ()
+    {
+        dl_binary_tree_node_base *x = this;
+        while ( x->m_right.get () )
+            x = x->m_right.get ();
+        return x;
+    }
+
+    dl_binary_tree_node_base *do_tree_increment () const;
+    dl_binary_tree_node_base *do_tree_decrement () const;
+
+    dl_binary_tree_node_base *rotate_left ();
+    dl_binary_tree_node_base *rotate_right ();
+    dl_binary_tree_node_base *rotate_to_parent ()
+    {
+        if ( is_left_child () )
+            return m_parent->rotate_right ();
+        else
+            return m_parent->rotate_left ();
+    }
+
+    bool is_left_child () const { return (m_parent ? this == m_parent->m_left.get () : false); }
+
+    bool is_linear () const { return m_parent && (is_left_child () == m_parent->is_left_child ()); }
+};
+
+}   // namespace containers
+}   // namespace red
---
+++ lib/src/tree_node.cc
@@ -0,0 +1,100 @@
+/* ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <alex.rom23@mail.ru> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Alex Romanov
+ * ----------------------------------------------------------------------------
+ */
+
+// defenitions for some functions declared in "tree_node.hpp"
+
+#pragma once
+
+#include "tree_node.hpp"
+
+namespace red
+{
+namespace containers
+{
+
+dl_binary_tree_node_base *dl_binary_tree_node_base::successor () const
+{
+    {
+        auto curr = this;
+        if ( curr->m_right )
+            return curr->m_right->minimum ();
+        auto *prev = curr->m_parent;
+        while ( prev->m_parent && !curr->is_left_child () )
+        {
+            curr = prev;
+            prev = prev->m_parent;
+        }
+        if ( prev->m_parent )
+            return prev;
+        return nullptr;
+    }
+}
+
+dl_binary_tree_node_base *dl_binary_tree_node_base::predecessor () const
+{
+    {
+        auto curr = this;
+        if ( curr->m_left )
+            return curr->m_left->maximum ();
+        auto prev = curr->m_parent;
+        while ( prev->m_parent && curr == prev->m_left )
+        {
+            curr = prev;
+            prev = prev->m_parent;
+        }
+        if ( prev->m_parent )
+            return prev;
+        return nullptr;
+    }
+}
+
+dl_binary_tree_node_base *dl_binary_tree_node_base::rotate_left ()
+{
+    auto node       = this;
+    auto parent     = this->m_parent;
+    auto rchild_ptr = node;
+    node->m_right   = rchild_ptr->m_left;
+    if ( node->m_right )
+        node->m_right->m_parent = node;
+
+    rchild_ptr->m_parent = parent;
+    if ( parent )
+    {
+        rchild_ptr->m_left = node;
+        if ( node->is_left_child () )
+            parent->m_left = rchild_ptr;
+        else
+            parent->m_right = rchild_ptr;
+    }
+    node->m_parent = rchild_ptr;
+    return rchild_ptr;
+}
+
+dl_binary_tree_node_base *dl_binary_tree_node_base::rotate_right ()
+{
+    auto node       = this;
+    auto parent     = this->m_parent;
+    auto lchild_ptr = node;
+    node->m_left    = lchild_ptr->m_right;
+    if ( node->m_left )
+        node->m_left->m_parent = node;
+    lchild_ptr->m_parent = parent;
+    if ( parent )
+    {
+        lchild_ptr->m_right = node;
+        if ( node->is_left_child () )
+            parent->m_left = lchild_ptr;
+        else
+            parent->m_right = lchild_ptr;
+    }
+    node->m_parent = lchild_ptr;
+    return lchild_ptr;
+}
+
+}   // namespace containers
+}   // namespace red
